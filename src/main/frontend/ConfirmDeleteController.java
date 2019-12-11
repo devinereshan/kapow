2a903b7e5eedac21f38e9f1f6a98342a227acb8e
Devin Ereshan <devinereshan@gmail.com>
2019-12-11 14:24:29 -0500

    Add controller to confirm delete fxml

---
+++ src/main/frontend/ConfirmDeleteController.java
@@ -0,0 +1,27 @@
+package main.frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ConfirmDeleteController {
+
+    @FXML
+    private Label trackLabel;
+
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    void cancelClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmClicked(ActionEvent event) {
+
+    }
+
+    
+
+}
